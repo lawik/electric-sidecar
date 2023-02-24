f7bbd327098501a2d952e7553958ddf8e4499b22
Lars Wikman <lars@underjord.io>
2023-02-24 12:02:36 +0100

    Fix config problem

--- lib/electric_sidecar.ex
+++ lib/electric_sidecar.ex
@@ -10,23 +10,25 @@ defmodule ElectricSidecar do
     db_path = Keyword.get(opts, :path)
     config_path = Keyword.get(opts, :config_path)
     pid = Keyword.get(opts, :pid)
-    script_path = "index.js"
+    [_preamble, config] = config_path |> File.read!() |> String.split("export default ")
+    script = EEx.eval_file("lib/sidecar.js.eex", config_json: config)
+    script_name = "index.js"
 
-    priv_path =
-      :code.priv_dir(:electric_sidecar)
-      |> IO.inspect()
-
+    priv_path = :code.priv_dir(:electric_sidecar)
     path = Path.join(priv_path, "node")
+    File.mkdir_p!(path)
+    script_path = Path.join(path, script_name)
+    File.write!(script_path, script)
+
     IO.inspect(db_path, label: "database")
     IO.inspect(config_path, label: "config")
     IO.inspect(script_path, label: "script")
     Process.flag(:trap_exit, true)
 
     port =
-      Port.open({:spawn, "node #{script_path} #{db_path} #{config_path}"}, [
+      Port.open({:spawn, "node #{script_path} #{db_path}"}, [
         :binary,
         {:packet, 4},
-        {:cd, path},
         :exit_status
       ])
 
